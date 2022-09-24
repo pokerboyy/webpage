841de608ee2d45eb7db01d87ddd24e831d884b6f
pokerboyy <preritchauhan123@gmail.com>
2022-09-24 19:49:11 +0530

    initial commit

---
+++ new.js
@@ -0,0 +1,9 @@
+<!DOCtYPE html>
+<html>
+    <body>
+        <button type="button"
+        onclick="document.getElementById('demo').innerHTML = Date()">
+            Cick me to display Date and Time.</button>"
+            <p id="demo"></p>
+    </body>
+</html>
